20507a2e9f1a307b36ee9f5ed6678726020d923f
brayan <brayanjaraba2006@gmail.com>
2026-03-30 14:10:11 -0500

    feat: return no div container if there is no label

--- src/components/CitySelect.jsx
+++ src/components/CitySelect.jsx
@@ -1,19 +1,34 @@
 export function CitySelect({ label = false, className = "" }) {
-  return (
-    <div className={`flex flex-col ${className}`}>
-      {label && (
+  if (label) {
+    return (
+      <div className={`flex flex-col ${className}`}>
+        label && (
         <label>
           Ciudad de residencia <span className="text-red-500">*</span>
         </label>
-      )}
-      <select name="city" className="border rounded-md h-fit p-1.5">
-        <option value="">Seleccionar ciudad</option>
-        <option value="Mediellin">Medellin</option>
-        <option value="">Bogota</option>
-        <option value="">Cali</option>
-        <option value="">Carepa</option>
-        <option value="">Chigorodo</option>
-      </select>
-    </div>
+        )
+        <select name="city" className="border rounded-md h-fit p-1.5">
+          <option value="">Seleccionar ciudad</option>
+          <option value="Mediellin">Medellin</option>
+          <option value="">Bogota</option>
+          <option value="">Cali</option>
+          <option value="">Carepa</option>
+          <option value="">Chigorodo</option>
+        </select>
+      </div>
+    );
+  }
+  return (
+    <select
+      name="city"
+      className={`border rounded-md h-fit p-1.5 ${className}`}
+    >
+      <option value="">Seleccionar ciudad</option>
+      <option value="Mediellin">Medellin</option>
+      <option value="">Bogota</option>
+      <option value="">Cali</option>
+      <option value="">Carepa</option>
+      <option value="">Chigorodo</option>
+    </select>
   );
 }
--- src/components/RoleSelect.jsx
+++ src/components/RoleSelect.jsx
@@ -1,17 +1,26 @@
-export function RoleSelect({ label = false }) {
-  return (
-    <div className="flex flex-col  ">
-      {label && (
+export function RoleSelect({ label = false, className = "" }) {
+  if (label) {
+    return (
+      <div className={`flex flex-col ${className}`}>
         <label>
           Rol <span className="text-red-500">*</span>
         </label>
-      )}
-      <select className="border rounded-md h-fit p-1.5">
-        <option value="">Seleccionar rol</option>
-        <option value="ESTUDIANTE">ESTUDIANTE</option>
-        <option value="PROFESOR">PROFESOR</option>
-        <option value="ADMINISTRADOR">ADMINSTRADOR</option>
-      </select>
-    </div>
+        )
+        <select className="border rounded-md h-fit p-1.5">
+          <option value="">Seleccionar rol</option>
+          <option value="ESTUDIANTE">ESTUDIANTE</option>
+          <option value="PROFESOR">PROFESOR</option>
+          <option value="ADMINISTRADOR">ADMINSTRADOR</option>
+        </select>
+      </div>
+    );
+  }
+  return (
+    <select className={`border rounded-md h-fit p-1.5 ${className}`}>
+      <option value="">Seleccionar rol</option>
+      <option value="ESTUDIANTE">ESTUDIANTE</option>
+      <option value="PROFESOR">PROFESOR</option>
+      <option value="ADMINISTRADOR">ADMINSTRADOR</option>
+    </select>
   );
 }
