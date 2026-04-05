49d88cff832614f1824662d023dd9c419c7220f4
brayan <brayanjaraba2006@gmail.com>
2026-04-05 15:11:47 -0500

    chroe: remove unused function and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Aouth } from "./screens/Aouth.jsx";
 import { TobBar } from "./components/TopBar.jsx";
 import { NewUser } from "./screens/NewUser.jsx";
 import { FilterUsers } from "./screens/FilterUsers.jsx";
+import { PrivateRoute } from "./components/PrivateRoute.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <TobBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Aouth />} />
-          <Route path="/users/new" element={<NewUser />} />
-          <Route path="/users" element={<FilterUsers />} />
+          <Route element={<PrivateRoute allowedRoles={["ADMINISTRADOR"]} />}>
+            <Route path="/users/new" element={<NewUser />} />
+            <Route path="/users" element={<FilterUsers />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
