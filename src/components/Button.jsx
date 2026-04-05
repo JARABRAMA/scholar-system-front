3c06efb59fde7110431db3a88adb224ff66bc0c1
brayan <brayanjaraba2006@gmail.com>
2026-04-05 16:11:24 -0500

    fix: make Button component can has custom onClicks calls

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
-export function Button({ className, children }) {
+export function Button({ onClick = () => {}, className, children }) {
   return (
     <button
+      onClick={onClick}
       className={`${className} transition-transform duration-300 hover:outline-2 active:scale-90 box-border py-1.5 rounded-md px-3`}
     >
       {children}
