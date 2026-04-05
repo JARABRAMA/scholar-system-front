53b0e1d24aa11d2ede27ddaaf91ba6edf24294af
brayan <brayanjaraba2006@gmail.com>
2026-04-05 16:12:29 -0500

    refactor:  set initial state out of the login state

--- src/store/LoginStore.jsx
+++ src/store/LoginStore.jsx
@@ -1,12 +1,16 @@
 import { create } from "zustand";
 import { jwtDecode } from "jwt-decode";
 
-export const useLoginStore = create((set, get, store) => ({
+const initialState = {
   fullname: undefined,
   email: undefined,
   role: undefined,
   id: undefined,
   accessToken: undefined,
+};
+
+export const useLoginStore = create((set, get, store) => ({
+  ...initialState,
 
   login: (token) => {
     const object = jwtDecode(token);
@@ -15,9 +19,9 @@ export const useLoginStore = create((set, get, store) => ({
       role: object.role,
       email: object.sub,
       id: object.jti,
-      token: token,
+      accessToken: token,
     }));
   },
 
-  logout: () => set(store.getInitialState()),
+  logout: () => set(initialState),
 }));
