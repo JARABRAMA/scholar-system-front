c1e8e38277c6f595f3b2fc379964024e64c5a5cc
brayan <brayanjaraba2006@gmail.com>
2026-04-06 10:53:26 -0500

    feat: make role select can handle his value chage

--- src/components/RoleSelect.jsx
+++ src/components/RoleSelect.jsx
@@ -1,12 +1,21 @@
-export function RoleSelect({ label = false, className = "" }) {
+export function RoleSelect({
+  label = false,
+  className = "",
+  onInput = () => {},
+}) {
   if (label) {
     return (
       <div className={`flex flex-col ${className}`}>
         <label>
           Rol <span className="text-red-500">*</span>
         </label>
-        )
-        <select className="border rounded-md h-fit p-1.5">
+
+        <select
+          onChange={(e) => {
+            onInput(e.target.value);
+          }}
+          className="border rounded-md h-fit p-1.5"
+        >
           <option value="">Seleccionar rol</option>
           <option value="ESTUDIANTE">ESTUDIANTE</option>
           <option value="PROFESOR">PROFESOR</option>
@@ -16,7 +25,12 @@ export function RoleSelect({ label = false, className = "" }) {
     );
   }
   return (
-    <select className={`border rounded-md h-fit p-1.5 ${className}`}>
+    <select
+      onChange={(e) => {
+        onInput(e.target.value);
+      }}
+      className={`border rounded-md h-fit p-1.5 ${className}`}
+    >
       <option value="">Seleccionar rol</option>
       <option value="ESTUDIANTE">ESTUDIANTE</option>
       <option value="PROFESOR">PROFESOR</option>
