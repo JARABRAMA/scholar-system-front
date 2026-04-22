15bca7054eb5f887848cb8dd1343917d7ee2addc
brayan <brayanjaraba2006@gmail.com>
2026-04-22 13:32:43 -0500

    creating courses

--- src/screens/CoursesScreen.jsx
+++ src/screens/CoursesScreen.jsx
@@ -1,8 +1,9 @@
 import { SideBar } from "../components/SideBar";
+import { Button } from "../components/Button";
 
 export function CoursesScreen() {
   return (
-    <main className="grid grid-cols-[auto_1fr] ">
+    <main className="grid grid-cols-[auto_1fr] bg-stone-100">
       <SideBar />
       <Content />
     </main>
@@ -26,28 +27,46 @@ function CoursesGird() {
       <CourseCard
         code="CS101"
         name="Introducción a la Programación"
+        ngroups={4}
         credits={3}
       />
       <CourseCard
         code="CS101"
         name="Introducción a la Programación"
+        ngroups={3}
         credits={3}
       />
       <CourseCard
         code="CS101"
         name="Introducción a la Programación"
+        ngroups={2}
         credits={3}
       />
     </section>
   );
 }
 
-function CourseCard({ code, name, credits }) {
+function CourseCard({ code, name, credits, ngroups }) {
   return (
-    <div className="bg-stone-100 rounded-md shadow-sm shadow-stone-300 border-2 border-stone-300 px-2.5 py-2">
-      <span>{code}</span>
-      <p>{name}</p>
-      <p>{credits} créditos</p>
+    <div
+      className="bg-white  gap-2 rounded-xl shadow-sm shadow-stone-100 
+     border-2 border-stone-300 px-4 py-4 flex flex-col "
+    >
+      <span className="text-stone-600">{code}</span>
+      <span className="text-lg text-black">{name}</span>
+      <span className="text-stone-600 text-sm">{ngroups} grupos</span>
+      <div className="flex justify-between">
+        <span className="bg-blue-100 w-fit px-1.5 py-0.5 rounded-lg text-blue-800">
+          {credits} créditos
+        </span>
+        <Button
+          className={
+            "w-fit h-fit border border-stone-300 rounded-xl text-stone-900"
+          }
+        >
+          Ver grupos
+        </Button>
+      </div>
     </div>
   );
 }
