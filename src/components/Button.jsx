ea9c50dc2a7160a972501ce6574e5988e1c70676
brayan <brayanjaraba2006@gmail.com>
2026-04-24 13:49:48 -0500

    feat: add type prop to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
-export function Button({ onClick = () => {}, className, children }) {
+export function Button({
+  onClick = () => {},
+  className,
+  type = "button",
+  children,
+}) {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={`${className} transition-transform duration-300 hover:outline-2 active:scale-90 box-border py-1.5 rounded-md px-3`}
     >
