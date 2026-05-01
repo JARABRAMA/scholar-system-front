6bac23dcbcd14ddd4c5297b556c82b8e0ba02d93
brayan <brayanjaraba2006@gmail.com>
2026-05-01 14:28:11 -0500

    feat: visual improvements for profile button

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useLoginStore } from "../store/LoginStore";
 
 export function SideBar() {
+  const location = useLocation();
   return (
     <aside
       className="flex flex-col gap-3 bg-stone-200
     py-6 px-3 shadow-sm shadow-stone-100 flex-1"
     >
-      <SideNav selected={true}>Gestionar Usuarios</SideNav>
-      <SideNav>Gestionar Cursos</SideNav>
+      <SideNav selected={location.pathname.startsWith("/users")} path="/users">
+        Gestionar Usuarios
+      </SideNav>
+      <SideNav
+        selected={location.pathname.startsWith("/courses")}
+        path="/courses"
+      >
+        Gestionar Cursos
+      </SideNav>
+      <div className="flex-1" />
       <ProfileLink />
     </aside>
   );
@@ -16,14 +25,32 @@ export function SideBar() {
 
 function ProfileLink() {
   const userId = useLoginStore((state) => state.id);
-  return <Link to={`/profile/${userId}`}>Perfil</Link>;
+  const location = useLocation();
+  console.log(location.pathname);
+  const selected = location.pathname.startsWith("/profile");
+  return (
+    <Link
+      className={
+        selected
+          ? "flex justify-self-end gap-3 py-2 px-4 bg-stone-50 shadow-sm rounded-xl"
+          : "flex justify-self-end gap-3 py-2 px-4"
+      }
+      to={`/profile/${userId}`}
+    >
+      <svg className="size-5">
+        <use href="/sprite.svg#user" />
+      </svg>
+      Perfil
+    </Link>
+  );
 }
 
-function SideNav({ selected, children, className }) {
+function SideNav({ selected, children, className, path = "" }) {
   const classSelected =
     "rounded-lg bg-white shadow-sm items-center text-black justify-start  text-sm";
   return (
     <Link
+      to={path}
       className={
         selected
           ? `flex gap-3 py-2 px-4 ${classSelected} ${className}`
