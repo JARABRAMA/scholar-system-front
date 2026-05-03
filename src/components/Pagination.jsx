6364c894e6084800c1b589396ad1d083499d7960
brayan <brayanjaraba2006@gmail.com>
2026-05-03 16:14:23 -0500

    refactor: separate pagination into a diferent file

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,47 @@
+export function Pagination({
+  onPreviousPage,
+  isFirstPage,
+  pages,
+  currentPage,
+  onSetPage,
+  onNextPage,
+  isLastPage,
+}) {
+  return (
+    <div className="flex justify-center gap-2 mt-4">
+      <button
+        onClick={onPreviousPage}
+        disabled={isFirstPage}
+        className={`flex items-center justify-center  rounded-xl size-10
+           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
+      >
+        <svg className="size-6">
+          <use href="/sprite.svg#navigate-previous" />
+        </svg>
+      </button>
+
+      {pages.map((p) => (
+        <button
+          disabled={currentPage + 1 === p}
+          key={p}
+          onClick={() => onSetPage(p - 1)}
+          className={`flex items-center justify-center  rounded-xl size-10
+           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-black `}
+        >
+          {p}
+        </button>
+      ))}
+
+      <button
+        onClick={onNextPage}
+        disabled={isLastPage}
+        className={`flex items-center justify-center  rounded-xl size-10
+           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
+      >
+        <svg className="size-6">
+          <use href="/sprite.svg#navigate-next" />
+        </svg>
+      </button>
+    </div>
+  );
+}
--- src/screens/FilterUsers.jsx
+++ src/screens/FilterUsers.jsx
@@ -5,6 +5,7 @@ import { useFilterUsers } from "../hooks/UseFilterUsers.jsx";
 import { Spinner } from "../components/Spinner.jsx";
 import { useNavigate } from "react-router";
 import { NavigationPaths } from "../navigation/NavigationPaths.jsx";
+import { Pagination } from "../components/Pagination.jsx";
 
 export function FilterUsers() {
   const {
@@ -177,41 +178,15 @@ function UsersList({
           )}
         </div>
       </section>
-      <div className="flex justify-center gap-2 mt-4">
-        <button
-          onClick={onPreviousPage}
-          disabled={isFirstPage}
-          className={`flex items-center justify-center  rounded-xl size-10
-           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
-        >
-          <svg className="size-6">
-            <use href="/sprite.svg#navigate-previous" />
-          </svg>
-        </button>
-
-        {pages.map((p) => (
-          <button
-            disabled={currentPage + 1 === p}
-            key={p}
-            onClick={() => onSetPage(p - 1)}
-            className={`flex items-center justify-center  rounded-xl size-10
-           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-black `}
-          >
-            {p}
-          </button>
-        ))}
-
-        <button
-          onClick={onNextPage}
-          disabled={isLastPage}
-          className={`flex items-center justify-center  rounded-xl size-10
-           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
-        >
-          <svg className="size-6">
-            <use href="/sprite.svg#navigate-next" />
-          </svg>
-        </button>
-      </div>
+      <Pagination
+        onPreviousPage={onPreviousPage}
+        isFirstPage={isFirstPage}
+        pages={pages}
+        currentPage={currentPage}
+        onSetPage={onSetPage}
+        onNextPage={onNextPage}
+        isLastPage={isLastPage}
+      />
     </div>
   );
 }
