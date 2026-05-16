8a4550b23a089ef9292637b423008e9a90bafd2c
brayan <brayanjaraba2006@gmail.com>
2026-05-16 12:25:13 -0500

    chore: format files fix: handle connexion errors

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,57 +1,58 @@
-import { Button } from "./Button";
-import { Link } from "react-router";
-import { Input } from "./Input";
-import { useLogin } from "../hooks/UseLogin.jsx";
-import { Spinner } from "../components/Spinner.jsx";
+import {Button} from "./Button";
+import {Link} from "react-router";
+import {Input} from "./Input";
+import {useLogin} from "../hooks/UseLogin.jsx";
+import {Spinner} from "./Spinner.jsx";
+
 export function LoginForm() {
-  const { onLogin, error, loading } = useLogin();
-  return (
-    <article
-      className="w-md rounded-2xl px-5  py-6 flex flex-col gap-3 
+	const {onLogin, error, loading} = useLogin();
+	return (
+		<article
+			className="w-md rounded-2xl px-5  py-6 flex flex-col gap-3
     bg-stone-200 border-2 border-stone-300 shadow-md shadow-stone-300/80"
-    >
-      <div>
-        <h2 className="text-3xl font-bold m-0 p-0">Bienvenido de Vuelta</h2>
-        <small className="text-sm text-gray-500">
-          Ingresa tus credenciales para continuar
-        </small>
-      </div>
+		>
+			<div>
+				<h2 className="text-3xl font-bold m-0 p-0">Bienvenido de Vuelta</h2>
+				<small className="text-sm text-gray-500">
+					Ingresa tus credenciales para continuar
+				</small>
+			</div>
 
-      <form className="flex flex-col gap-4" onSubmit={onLogin}>
-        <Input
-          name="email"
-          placeholder="user@gmail.com"
-          type="email"
-          required={true}
-        >
-          Correo Electronico
-        </Input>
+			<form className="flex flex-col gap-4" onSubmit={onLogin}>
+				<Input
+					name="email"
+					placeholder="user@gmail.com"
+					type="email"
+					required={true}
+				>
+					Correo Electronico
+				</Input>
 
-        <Input
-          name="password"
-          placeholder="******"
-          type="password"
-          required={true}
-        >
-          Contraseña
-        </Input>
-        {loading && (
-          <div className="flex items-center justify-center">
-            <Spinner />
-          </div>
-        )}
-        {error && <span className="text-red-400 self-center">{error}</span>}
-        <Button
-          type="submit"
-          className="bg-blue-700 text-gray-100 hover:bg-blue-600 border-0 hover:outline-0"
-        >
-          Iniciar Sesión
-        </Button>
+				<Input
+					name="password"
+					placeholder="******"
+					type="password"
+					required={true}
+				>
+					Contraseña
+				</Input>
+				{loading && !error && (
+					<div className="flex items-center justify-center">
+						<Spinner/>
+					</div>
+				)}
+				{error && <span className="text-red-400 self-center">{error}</span>}
+				<Button
+					type="submit"
+					className="bg-blue-700 text-gray-100 hover:bg-blue-600 border-0 hover:outline-0"
+				>
+					Iniciar Sesión
+				</Button>
 
-        <Link className="self-center text-blue-700 hover:underline">
-          ¿Olvidaste tu contraseña?
-        </Link>
-      </form>
-    </article>
-  );
+				<Link className="self-center text-blue-700 hover:underline">
+					¿Olvidaste tu contraseña?
+				</Link>
+			</form>
+		</article>
+	);
 }
--- src/hooks/UseLogin.jsx
+++ src/hooks/UseLogin.jsx
@@ -1,36 +1,40 @@
-import { useNavigate } from "react-router";
-import { useLoginStore } from "../store/LoginStore.jsx";
-import { useState } from "react";
+import {useNavigate} from "react-router";
+import {useLoginStore} from "../store/LoginStore.jsx";
+import {useState} from "react";
 
 export function useLogin() {
-  const navigate = useNavigate();
-  const login = useLoginStore((state) => state.login);
-  const baseUrl = import.meta.env.VITE_BASE_URL;
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const login = useLoginStore((state) => state.login);
+	const baseUrl = import.meta.env.VITE_BASE_URL;
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
 
-  const onLogin = async (event) => {
-    setLoading(true);
-    event.preventDefault();
-    const formData = new FormData(event.target);
+	const onLogin = async (event) => {
+		setLoading(true);
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		try {
 
-    const res = await fetch(`${baseUrl}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Object.fromEntries(formData)),
-    });
+			const res = await fetch(`${baseUrl}/auth/login`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(Object.fromEntries(formData)),
+			});
 
-    const data = await res.json();
+			const data = await res.json();
 
-    if (res.ok) {
-      login(data.accessToken);
-      navigate("/users");
-      return;
-    }
-    setLoading(false);
-    setError(data.detail);
-  };
-  return { onLogin, error, loading };
+			if (res.ok) {
+				login(data.accessToken);
+				navigate("/users");
+				return;
+			}
+			setLoading(false);
+			setError(data.detail);
+		} catch (e) {
+			setError("Error de conexión por favor intenta más tarde")
+		}
+	}
+	return {onLogin, error, loading};
 }
