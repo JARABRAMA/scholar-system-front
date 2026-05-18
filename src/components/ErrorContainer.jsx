cbfa4c4ea213f3a6a58a2a5d02e30e66b950efe7
brayan <brayanjaraba2006@gmail.com>
2026-05-18 18:07:16 -0500

    feat: improve the error container visual

--- src/components/ErrorContainer.jsx
+++ src/components/ErrorContainer.jsx
@@ -1,12 +1,16 @@
 export function ErrorContainer ({ error }) {
   return (
     <div className='flex flex-col justify-center gap-6 flex-1 items-center justify-centertext
-		 text-red-400 text-xl'
+		 text-xl'
     >
-      <svg className='size-16 text-red-500'>
-        <use href='/sprite.svg#warning' />
-      </svg>
-      <p>{error}</p>
+
+      <h2 className='text-2xl font-bold text-red-500 flex flex-col items-center'>
+        <svg className='size-16 text-red-500'>
+          <use href='/sprite.svg#warning' />
+        </svg>
+        Error
+      </h2>
+      <p className='text-stone-800'>{error}</p>
     </div>
   )
 }
