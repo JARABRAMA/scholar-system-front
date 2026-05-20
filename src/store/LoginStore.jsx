49740d742e0a7abff98d4f23ed202b07662d6655
brayan <brayanjaraba2006@gmail.com>
2026-05-20 14:32:19 -0500

    format file

--- src/store/LoginStore.jsx
+++ src/store/LoginStore.jsx
@@ -1,27 +1,27 @@
-import { create } from "zustand";
-import { jwtDecode } from "jwt-decode";
+import { create } from 'zustand'
+import { jwtDecode } from 'jwt-decode'
 
 const initialState = {
   fullname: undefined,
   email: undefined,
   role: undefined,
   id: undefined,
-  accessToken: undefined,
-};
+  accessToken: undefined
+}
 
 export const useLoginStore = create((set, get, store) => ({
   ...initialState,
 
   login: (token) => {
-    const object = jwtDecode(token);
+    const object = jwtDecode(token)
     set(() => ({
       fullname: object.name,
       role: object.role,
       email: object.sub,
       id: object.jti,
-      accessToken: token,
-    }));
+      accessToken: token
+    }))
   },
 
-  logout: () => set(initialState),
-}));
+  logout: () => set(initialState)
+}))
