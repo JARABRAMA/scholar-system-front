10c87ce87bcf017d2b10f654392994805ceefb44
brayan <brayanjaraba2006@gmail.com>
2026-05-20 14:30:24 -0500

    format file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { Aouth } from "./screens/Aouth.jsx";
-import { TobBar } from "./components/TopBar.jsx";
-import { NewUser } from "./screens/NewUser.jsx";
-import { FilterUsers } from "./screens/FilterUsers.jsx";
-import { PrivateRoute } from "./components/PrivateRoute.jsx";
-import { NavigationPaths } from "./navigation/NavigationPaths.jsx";
-import { CoursesScreen } from "./screens/CoursesScreen.jsx";
-import { ProfileScreen } from "./screens/ProfileScreen.jsx";
-import { EditUser } from "./screens/EditUser.jsx";
-import { NewCourse } from "./screens/NewCourseScreen.jsx";
-import { CourseDetails } from "./screens/CourseDetails.jsx";
-import { NewGroupScreen } from "./screens/NewGroupScreen.jsx";
-import { GroupDetails } from "./screens/GroupDetails.jsx";
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { Aouth } from './screens/Aouth.jsx'
+import { TobBar } from './components/TopBar.jsx'
+import { NewUser } from './screens/NewUser.jsx'
+import { FilterUsers } from './screens/FilterUsers.jsx'
+import { PrivateRoute } from './components/PrivateRoute.jsx'
+import { NavigationPaths } from './navigation/NavigationPaths.jsx'
+import { CoursesScreen } from './screens/CoursesScreen.jsx'
+import { ProfileScreen } from './screens/ProfileScreen.jsx'
+import { EditUser } from './screens/EditUser.jsx'
+import { NewCourse } from './screens/NewCourseScreen.jsx'
+import { CourseDetails } from './screens/CourseDetails.jsx'
+import { NewGroupScreen } from './screens/NewGroupScreen.jsx'
+import { GroupDetails } from './screens/GroupDetails.jsx'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
       <TobBar />
       <Routes>
-        <Route path="/" element={<Aouth />} />
-        <Route element={<PrivateRoute allowedRoles={["ADMINISTRADOR"]} />}>
+        <Route path='/' element={<Aouth />} />
+        <Route element={<PrivateRoute allowedRoles={['ADMINISTRADOR']} />}>
           <Route path={NavigationPaths.NEW_USER} element={<NewUser />} />
           <Route path={NavigationPaths.USERS} element={<FilterUsers />} />
           <Route path={NavigationPaths.PROFILE} element={<ProfileScreen />} />
@@ -42,7 +42,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
