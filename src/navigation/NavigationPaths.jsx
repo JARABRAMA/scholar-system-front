8516a3d609ec11f544dcf197b68acb590f151a9f
brayan <brayanjaraba2006@gmail.com>
2026-05-20 11:32:55 -0500

    format file

--- src/navigation/NavigationPaths.jsx
+++ src/navigation/NavigationPaths.jsx
@@ -1,12 +1,12 @@
 export const NavigationPaths = {
-  NEW_USER: "/users/new",
-  USERS: "/users",
-  LOGIN: "/",
-  COURSES: "/courses",
-  NEW_COURSE: "/courses/new",
-  COURSE_DETAIL: "/courses/:courseId",
-  PROFILE: "/profile/:id",
-  EDIT_USER: "/users/edit/:id",
-  NEW_GROUP: "/courses/:courseId/new-group",
-  GROUP_DETAIL: "/groups/:id",
-};
+  NEW_USER: '/users/new',
+  USERS: '/users',
+  LOGIN: '/',
+  COURSES: '/courses',
+  NEW_COURSE: '/courses/new',
+  COURSE_DETAIL: '/courses/:courseId',
+  PROFILE: '/profile/:id',
+  EDIT_USER: '/users/edit/:id',
+  NEW_GROUP: '/courses/:courseId/new-group',
+  GROUP_DETAIL: '/groups/:id'
+}
