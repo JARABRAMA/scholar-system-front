83c6c4a19cffd72d6181836d8dc0190685352a0b
brayan <brayanjaraba2006@gmail.com>
2026-05-20 11:32:47 -0500

    feat: add delete user functionality

--- src/components/useDeleteUser.jsx
+++ src/components/useDeleteUser.jsx
@@ -1,8 +1,17 @@
 import { useRef, useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import { useLoginStore } from '../store/LoginStore'
+import { NavigationPaths } from '../navigation/NavigationPaths'
 
 export function useDeleteUser () {
   const [showDeleteDialog, setShowDeleteDialog] = useState()
+  const [loadingDelete, setLoadingDelete] = useState(false)
+  const [errorDelete, setErrorDelete] = useState()
+  const { id } = useParams()
+  const accessToken = useLoginStore(state => state.accessToken)
   const deleteDialogRef = useRef()
+  const navigate = useNavigate()
+
   const onToggleShowDeleteDialog = () => {
     showDeleteDialog
       ? setShowDeleteDialog(false)
@@ -17,5 +26,40 @@ export function useDeleteUser () {
     }
   }, [showDeleteDialog])
 
-  return { deleteDialogRef, onToggleShowDeleteDialog }
+  const onDeleteUser = async () => {
+    setLoadingDelete(true)
+    try {
+      const serviceUrl = import.meta.env.VITE_BASE_URL
+      const url = `${serviceUrl}/users/${id}`
+      console.log('url: ', url)
+      const res = await fetch(url, {
+
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      if (res.ok) {
+        setLoadingDelete(false)
+        navigate(NavigationPaths.USERS)
+      } else {
+        const data = await res.json()
+        setErrorDelete(data.detail)
+      }
+    } catch (e) {
+      setErrorDelete('Error de conexión por favor intenta más tarde')
+      setLoadingDelete(false)
+    }
+  }
+
+  const onDissmissDeletingError = () => setErrorDelete(undefined)
+
+  return {
+    deleteDialogRef,
+    onToggleShowDeleteDialog,
+    onDeleteUser,
+    loadingDelete,
+    errorDelete,
+    onDissmissDeletingError
+  }
 }
