19db3309a6513409ca085638f124db332645e9fc
brayan <brayanjaraba2006@gmail.com>
2026-05-22 11:46:21 -0500

    fix: logic to hide content to non administrators users

--- src/hooks/useAdministrator.jsx
+++ src/hooks/useAdministrator.jsx
@@ -1,8 +1,10 @@
-import { useLoginStore } from '../store/LoginStore.jsx'
-import { Roles } from '../utils/Roles.js'
+import {useLoginStore} from '../store/LoginStore.jsx'
+import {Roles} from '../utils/Roles.js'
 
 export const useAdministrator = () => {
   const role = useLoginStore(state => state.role)
-  const isAdmin = role === Roles.ADMINISTRADO
+  console.log('role ', role)
+
+  const isAdmin = role === Roles.ADMINISTRADOR
   return { isAdmin }
 }
--- src/screens/CoursesScreen.jsx
+++ src/screens/CoursesScreen.jsx
@@ -1,16 +1,16 @@
-import { SideBar } from '../components/SideBar.jsx'
-import { AdministratorContent } from '../components/course/AdministratorContent.jsx'
-import { TeacherStudentContent } from '../components/course/TeacherStudentContent.jsx'
-import { useAdministrator } from '../hooks/useAdministrator.jsx'
+import {SideBar} from '../components/SideBar.jsx'
+import {AdministratorContent} from '../components/course/AdministratorContent.jsx'
+import {TeacherStudentContent} from '../components/course/TeacherStudentContent.jsx'
+import {useAdministrator} from '../hooks/useAdministrator.jsx'
 
 export function CoursesScreen () {
-  const { isAdministrator } = useAdministrator()
+  const { isAdmin } = useAdministrator()
   return (
     <main className='grid grid-cols-[auto_1fr] bg-stone-100 overflow-y-hidden'>
       <SideBar />
-      {isAdministrator &&
+      {isAdmin &&
         <AdministratorContent />}
-      {!isAdministrator &&
+      {!isAdmin &&
         <TeacherStudentContent />}
     </main>
   )
