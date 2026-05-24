73462c52e32e2caba602797a70ef8af926e40c87
brayan <brayanjaraba2006@gmail.com>
2026-05-24 16:09:44 -0500

    feat: add pagination interaction to buttons

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -12,7 +12,7 @@ export function Pagination ({
       <button
         onClick={onPreviousPage}
         disabled={isFirstPage}
-        className={`flex items-center justify-center  rounded-xl size-10
+        className={`flex items-center justify-center hover:ring-stone-600 rounded-xl size-10 hover:ring-2
            bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
       >
         <svg className='size-6'>
@@ -25,8 +25,9 @@ export function Pagination ({
           disabled={currentPage + 1 === p}
           key={p}
           onClick={() => onSetPage(p - 1)}
-          className={`flex items-center justify-center  rounded-xl size-10
-           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-black `}
+          className={`flex items-center hover:ring-2 justify-center  rounded-xl size-10
+           bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-black
+            disabled:hover:ring-2 ring-stone-800`}
         >
           {p}
         </button>
@@ -35,7 +36,7 @@ export function Pagination ({
       <button
         onClick={onNextPage}
         disabled={isLastPage}
-        className={`flex items-center justify-center  rounded-xl size-10
+        className={`flex items-center hover:ring-2 hover:ring-stone-600 justify-center  rounded-xl size-10
            bg-blue-500 text-white  shadow-sm duration-300 active:scale-[.9] disabled:opacity-50 disabled:bg-white disabled:border disabled:text-black`}
       >
         <svg className='size-6'>
