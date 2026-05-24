07e29c6ee5c43938671406b7e60e210f1e7415f0
brayan <brayanjaraba2006@gmail.com>
2026-05-24 16:07:55 -0500

    feat: add icons to side navigation links

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,6 @@
-import { Link, useLocation } from 'react-router'
-import { useLoginStore } from '../store/LoginStore.jsx'
-import { useSideBar } from '../hooks/useSideBar.jsx'
+import {Link, useLocation} from 'react-router'
+import {useLoginStore} from '../store/LoginStore.jsx'
+import {useSideBar} from '../hooks/useSideBar.jsx'
 
 export function SideBar () {
   const { location, isAdministrator } = useSideBar()
@@ -11,7 +11,12 @@ export function SideBar () {
     >
       {isAdministrator() &&
         <SideNav selected={location.pathname.startsWith('/users')} path='/users'>
-          Gestionar Usuarios
+          <div className='flex gap-1 items-center '>
+            <svg className='size-5'>
+              <use href='/sprite.svg#users'/>
+            </svg>
+            Usuarios
+          </div>
         </SideNav>}
       <SideNav
         selected={
@@ -20,7 +25,12 @@ export function SideBar () {
         }
         path='/courses'
       >
-        Gestionar Cursos
+        <div className='flex gap-1 items-center '>
+          <svg className='size-5'>
+            <use href='/sprite.svg#book'/>
+          </svg>
+          Cursos
+        </div>
       </SideNav>
       <div className='flex-1' />
       <ProfileLink />
@@ -32,12 +42,15 @@ function ProfileLink () {
   const userId = useLoginStore((state) => state.id)
   const location = useLocation()
   const selected = location.pathname.startsWith('/profile')
+  const classSelected =
+    'rounded-xl box-border hover:scale-105 bg-white duration-100 hover:border shadow-sm items-center text-black justify-start'
+
   return (
     <Link
       className={
         selected
-          ? 'flex justify-self-end gap-3 py-2 px-4 bg-stone-50 shadow-sm rounded-xl'
-          : 'flex justify-self-end gap-3 py-2 px-4'
+          ? `flex gap-3 py-2 box-border px-4 ${classSelected} `
+          : `flex gap-3 py-2 px-4 box-border duration-100 hover:bg-stone-300 hover:border rounded-xl hover:text-stone-800 hover:scale-105 items-center text-stone-500`
       }
       to={`/profile/${userId}`}
     >
@@ -51,14 +64,14 @@ function ProfileLink () {
 
 function SideNav ({ selected, children, className, path = '' }) {
   const classSelected =
-    'rounded-lg bg-white shadow-sm items-center text-black justify-start'
+    'rounded-xl box-border hover:scale-105 bg-white duration-100 hover:border shadow-sm items-center text-black justify-start'
   return (
     <Link
       to={path}
       className={
         selected
-          ? `flex gap-3 py-2 px-4 ${classSelected} ${className}`
-          : `flex gap-3 py-2 px-4 items-center text-stone-500 ${className}`
+          ? `flex gap-3 py-2 box-border px-4 ${classSelected} ${className}`
+          : `flex gap-3 py-2 px-4 box-border duration-100 hover:bg-stone-300 hover:border rounded-xl hover:text-stone-800 hover:scale-105 items-center text-stone-500 ${className}`
       }
     >
       <div
